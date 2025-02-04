4f51d31cedc5bdc18988456255ba4ebd8966c25e
Sinco <sinconsistencia@gmail.com>
2025-02-04 09:47:48 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,79 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "pizza":return img`
+. . . . . . f f f f f f . . . . 
+. . . . . f e e e e e e f . . . 
+. . . . f e e e e e e e e f . . 
+. . . . f e e e e e e e e f . . 
+. . . . . f f e e e e e e e f . 
+. . . . f 2 2 f e e e e e e f . 
+. . . f 5 5 5 5 f e e e e e f . 
+. . f 2 5 5 2 2 5 f e e e e f . 
+. . f 2 5 5 2 2 5 5 f e e e f . 
+. f 5 5 5 5 5 5 5 2 f e e e f . 
+. f 5 5 2 2 5 5 f f f e e e f . 
+. f 2 5 2 2 f f . . . f f f . . 
+. f 2 f f f . . . . . . . . . . 
+. f f . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "player":
+            case "anim1":return [img`
+. . f f f . . . . . . . f f f . 
+. . f f f . . . . . . . f f f . 
+. . . f . . f f f f f f . f . . 
+. . . f f f 7 7 7 7 7 7 f f . . 
+. . . f f 7 7 7 7 7 7 7 7 7 f . 
+. . . f f 7 7 7 7 7 7 7 7 7 f . 
+. . . f f 7 7 7 7 f 1 7 1 f f . 
+. . . f f 7 7 7 7 f 1 7 1 f f . 
+. . . f 7 7 7 7 7 f f 7 f f f . 
+. . . f 7 7 7 7 7 7 7 7 7 7 f . 
+. . . . f 7 7 f 7 7 7 7 7 f . . 
+. . . . f 7 7 f f f f f f f . . 
+. . . . . f 7 7 7 7 7 7 f . . . 
+. . . . . . f f f f f f . . . . 
+. . . . . . . f f f f . . . . . 
+. . . . . . . f f f f . . . . . 
+`, img`
+. . f f . . . . . . . . . . f f 
+. f f f . . . . . . . . . . f f 
+. f f f . . f f f f f f . . f . 
+. . . f f f 7 7 7 7 7 7 f f . . 
+. . . f 7 7 7 7 7 7 7 7 7 7 f . 
+. . . f f 7 7 7 7 7 7 7 7 7 f . 
+. . . f f 7 7 7 7 f 1 7 1 f f . 
+. . . f f 7 7 7 7 f 1 7 1 f f . 
+. . . f 7 7 7 7 7 f f 7 f f f . 
+. . . f 7 7 7 7 7 7 7 7 7 7 f . 
+. . . . f 7 7 f 7 7 7 7 7 f . . 
+. . . . f 7 7 f f f f f f f . . 
+. . f f f f 7 7 7 7 7 7 f . . . 
+. . f f . . f f f f f f . f f . 
+. . f f . . . . . . . f f f f . 
+. . . . . . . . . . . . f f . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
